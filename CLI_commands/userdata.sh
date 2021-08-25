85bbc58634e1478c51943129759c78115421808d
Yasemin <gonenyasemin@gmail.com>
2021-08-25 16:40:10 +0300

    clı-command

--- CLI_commands/userdata.sh
+++ CLI_commands/userdata.sh
@@ -3,10 +3,10 @@ yum update -y
 yum install python3 -y
 pip3 install flask
 cd /home/ec2-user
-wget https://raw.githubusercontent.com/serdarcw/my-repository/master/Project-001-Roman-Numerals-Converter/app.py
+wget https://raw.githubusercontent.com/yasemingonen/Roman-Numerals-Converter/main/app.py
 mkdir templates
 cd templates
-wget https://raw.githubusercontent.com/serdarcw/my-repository/master/Project-001-Roman-Numerals-Convertetemplates/index.html
-wget https://raw.githubusercontent.com/serdarcw/my-repository/master/Project-001-Roman-Numerals-Convertetemplates/result.html
+wget https://raw.githubusercontent.com/yasemingonen/Roman-Numerals-Converter/main/templates/index.html
+wget https://raw.githubusercontent.com/yasemingonen/Roman-Numerals-Converter/main/templates/result.html
 cd ..
 python3 app.py
